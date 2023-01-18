18db567feaca1a9e75bf95b802da7a9c16687337
mbecel <mael1.becel@epitech.eu>
2023-01-18 15:59:55 +0100

    feat (lib/,src/,include/): Add lib, include and main

---
+++ include/my_3dprintf.h
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2022
+** my_3dprintf.c
+** File description:
+** my_3dprintf
+*/
+
+#ifndef MY_3DPRINTF
+    #define MY_3DPRINTF
+    #include "my.h"
+    #include "printf.h"
+    #include "formatstring.h"
+    #include "parser.h"
+
+    #include <stdbool.h>
+    #include <stddef.h>
+    #include <stdio.h>
+    #include <stdlib.h>
+
+#endif
